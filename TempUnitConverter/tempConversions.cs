3f93a32af9a89aa1f526c66496c972bef6231f8c
haku-kiro <botPrime@yahoo.com>
2017-03-18 16:00:23 +0200

    adding the files

---
+++ TempUnitConverter/tempConversions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempUnitConverter
+{
+    public class tempConversions
+    {
+        //delegate for temp conv useage
+        public delegate double conv(double val); //can ppotentially store a list of methods, with this sig
+
+        //properties or state
+
+
+        //methods or behaviour
+        public static double fromCtoF(double cIn)
+        {
+            return (cIn * 1.8) + 32;
+        }
+
+        public static double fromFtoC(double fIn)
+        {
+            return (fIn - 32) * 5 / 9;
+        }
+
+        //make a method that the delegate can contain so that it returns either C or F
+
+        public static void something()
+        {
+            Console.WriteLine("Something");
+        }
+        //events
+    }
+
+    public delegate void test();
+}
---
+++ testPlatform/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TempUnitConverter;
+
+namespace testPlatform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           tempConversions.conv handler = new tempConversions.conv(tempConversions.fromCtoF); //creating the delegate giving it the ctof default
+
+           //test handlerFromNamespace = new test(tempConversions.something); //namespace delegate
+
+            Console.WriteLine("Make a choice of unit of temp to convert to");
+            //promt user:
+            Console.WriteLine("Convert from: ");
+            string sFrom = (Console.ReadLine()).ToUpper(); //just making it upper
+            Console.WriteLine("Convert to: ");
+            string sTo = Console.ReadLine().ToUpper(); 
+
+            double tempIn;
+            double tempOut;
+
+            //determine which formula to apply based on from and to var
+            if (sFrom == "C" && sTo == "F")
+            {
+                Console.WriteLine("Please enter a temp in C:");
+                if (double.TryParse(Console.ReadLine(), out tempIn))
+                {
+                    tempOut = handler(tempIn); //delegate by default has the ctof method
+                    Console.WriteLine("The temp in F is: {0} F", tempOut); //do some error checking here
+                }
+                else
+                {
+                    Console.WriteLine("You entered an invalid value");
+                }
+            }
+            else if (sFrom == "F" && sTo == "C")
+            {
+                handler -= tempConversions.fromCtoF; //take out the cToF method (not using this conversion)
+                handler += tempConversions.fromFtoC; //add the f to c conversion
+                Console.WriteLine("Please enter a temp in F");
+                if (double.TryParse(Console.ReadLine(), out tempIn))
+                {
+                    tempOut = handler(tempIn);
+                    Console.WriteLine("The temp in C is: {0} C", tempOut);
+                }
+            }
+            else
+            {
+                Console.WriteLine("You have entered an invalid input.");
+            }
+
+            Console.ReadKey(); //so that it doesnt close ye (WAITS FOR INPUT)
+
+        }
+    }
+}
+
+
+
+//notes, maybe mod the delegate so that it is more useful in a sense that the class has methods that cw the C or the F for ex
+//so you can execute the delegate at the end - as a one liner
